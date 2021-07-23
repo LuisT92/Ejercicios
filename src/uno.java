c7d8f8428beb95111f0c9f7738bdfb9139a47a0d
Luis Humberto Torres <a2209978@correo.uia.mx>
2021-07-22 20:55:15 -0500

    Initial commit

---
+++ src/uno.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class uno {
+    public static void main (String []args) {
+       /* double numero = Math.random();
+        System.out.println(numero); */
+
+        //ejerccicio_1
+
+
+        Scanner entrada = new Scanner(System.in);
+       /*
+        float n,n2,n3;
+        float suma, promedio;
+        System.out.println("Ingresa las notas a promediar: ");
+        n = entrada.nextFloat();
+        n2 = entrada.nextFloat();
+        n3 = entrada.nextFloat();
+        suma = n+n2+n3;
+        promedio = suma/3;
+        System.out.println("La suma es: "+suma);
+        System.out.println("El promedio es: "+promedio);
+
+
+        float salarioSemanal, horasTrabajadas, salarioHora;
+        System.out.println("Ingresa las horas laboradas:");
+        horasTrabajadas = entrada.nextFloat();
+        System.out.println("Ingress su salario por hora:");
+        salarioHora = entrada.nextFloat();
+        salarioSemanal = salarioHora*horasTrabajadas;
+        System.out.println(" ");
+        System.out.println("Su salario En la semana es: " + salarioSemanal);
+
+        float guillermo, luis, juan, totalDeLosTres;
+
+        System.out.println("Ingresa la cantidad de dolares que tiene Guillermo: ");
+        guillermo = entrada.nextFloat();
+
+        luis = guillermo / 2;
+        juan = (luis+guillermo)/2;
+        totalDeLosTres = guillermo+luis+juan;
+        System.out.println("En total los tres poseen: "+totalDeLosTres);
+
+
+        float  slarioBase = 1000, comisionVenta = 150, salarioMensualTotal, autoComision, autos;
+
+        System.out.println("Cuantos autos vendio?");
+        autos = entrada.nextFloat();
+        comisionVenta *= autos;
+        System.out.println("ingresa el total del valor de los autos vendidos:");
+        autoComision = entrada.nextFloat();
+        autoComision*= 0.05;
+        salarioMensualTotal = slarioBase + autoComision +comisionVenta;
+        System.out.println("Su salario mensual es de: "+ salarioMensualTotal);
+
+
+
+        float participacion, primerParcial, segundoParcial, examenFinal;
+        double calificacionFinal;
+        System.out.println("Calificacion Participacion");
+        participacion = entrada.nextFloat();
+        System.out.println("Calificacion Primer Parcial");
+        primerParcial = entrada.nextFloat();
+        System.out.println("Calificacion Segundo Parcial");
+        segundoParcial = entrada.nextFloat();
+        System.out.println("Calificacion Examen Final");
+        examenFinal = entrada.nextFloat();
+        calificacionFinal = (participacion * 0.1) + (primerParcial * 0.25) + (segundoParcial * 0.25) +(examenFinal * .4);
+        System.out.println("Calificacion Final: "+ calificacionFinal);
+
+
+        float numero1, numero2;
+        double resultado;
+        System.out.println("Ingresa el valor de los numeros a sumar");
+        numero1 = entrada.nextFloat();
+        numero2 = entrada.nextFloat();
+        resultado = Math.pow(numero1, 2) + Math.pow(numero2, 2) + 2*(numero1*numero2);
+        System.out.println("El resultado de (a+b)2 es:"+resultado);
+
+        */
+
+    }
+}
